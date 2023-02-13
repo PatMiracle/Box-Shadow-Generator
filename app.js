a31f052148d82fe5d461acfead56f1bb97ed6da4
unknown <miraclepatidehen@gmail.com>
2023-02-13 02:43:09 -0800

    updated readme

---
+++ app.js
@@ -0,0 +1,88 @@
+// Select DOM
+const box = document.querySelector(".box")
+const inputs = [...document.querySelectorAll("input")]
+
+const shadowCode = document.querySelector(".shadow-code")
+const boxCode = document.querySelector(".box-code")
+
+const copyShadowCode = document.querySelector(".copy-shadow-code")
+const copyBoxCode = document.querySelector(".copy-box-code")
+
+// display code on load
+window.addEventListener("DOMContentLoaded", renderBox)
+
+// get elements value by id
+function getValue(selection) {
+  const element = document.getElementById(selection).value
+  if (element) {
+    return element
+  }
+
+  throw new Error(`your selection "${selection}" does not exist`)
+}
+
+// input event listener
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    renderBox()
+  })
+})
+
+// Render Box
+function renderBox() {
+  // Customize Shadow
+  const hOffset = getValue("h-offset")
+  const vOffset = getValue("v-offset")
+  const blurRadius = getValue("blur-radius")
+  const spreadRadius = getValue("spread-radius")
+  const shadowColorOpacity = getValue("opacity")
+  const inset = document.getElementById("inset").checked
+  let color = getValue("color")
+
+  color = hexToRgba(color, shadowColorOpacity)
+
+  let boxShadow = `${hOffset}px ${vOffset}px ${blurRadius}px ${spreadRadius}px ${color}`
+  if (inset) {
+    boxShadow = `inset ${hOffset}px ${vOffset}px ${blurRadius}px ${spreadRadius}px ${color}`
+  }
+
+  box.style.boxShadow = boxShadow
+  shadowCode.textContent = `box-shadow: ${boxShadow};`
+
+  //Adjust Box
+  const bgColor = getValue("bg-color")
+  const width = getValue("width")
+  const height = getValue("height")
+  const borderRadius = getValue("border-radius")
+
+  box.style.background = bgColor
+  box.style.width = `${width}em`
+  box.style.height = `${height}em`
+  box.style.borderRadius = `${borderRadius}em`
+
+  boxCode.textContent = `.box{
+    background-color: ${bgColor};
+    width: ${width}em;
+    height: ${height}em;
+    border-radius: ${borderRadius}em;
+  }`
+}
+
+// convert from hex to rgba
+function hexToRgba(color, shadowColorOpacity) {
+  let r = parseInt(color.substr(1, 2), 16)
+  let g = parseInt(color.substr(3, 2), 16)
+  let b = parseInt(color.substr(5, 2), 16)
+  return `rgba(${r}, ${g}, ${b}, ${shadowColorOpacity})`
+}
+
+// Copy code
+function copy(code) {
+  code.addEventListener("click", () => {
+    code.previousElementSibling.select()
+    document.execCommand("copy")
+    alert("code copied to your clipboard")
+  })
+}
+copy(copyShadowCode)
+copy(copyBoxCode)
---
+++ readme.md
@@ -0,0 +1,88 @@
+# Box Shadow Generator
+
+Customize awesome box shadows and get the CSS code.
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [What is box shadow](#what-is-box-shadow)
+  - [Live Site](#live-site)
+- [HTML Structure](#html-structure)
+- [Responsiveness](#responsiveness)
+  - [Small screen](#small-screen)
+  - [Big screen](#big-screen)
+- [Event Listeners](#event-listeners)
+
+## Overview
+
+This app was built with HTML, CSS and JavaScript.
+I would be talking about my process below.
+
+### What is box Shadow?
+
+The "box-shadow" is a CSS property that allows you to add shadow effects aroound an element's frame.
+
+In the code below, the first and second value (6px and 10px) are the X and Y offsets. The third and fourth values (19px and -9px) are the blur and spread radius. The last is the color.
+
+```css
+.box {
+  box-shadow: 6px 10px 19px -9px rgba(0, 0, 0, 0.7);
+}
+```
+
+![](./screenshots/css%20result.jpg)
+
+The keyword inset is used to specify to make the box-shadow inwards.
+
+### Live Site
+
+Live Site URL: https://www.box-shadow-generator.netlify.app
+
+## HTML Structure
+
+Within the body tags, the HTML Structure is as thus:
+
+- header
+- main
+  - section.box-center
+    - div.box
+  - section.customize
+    - div.title
+    - div.inputs-wrapper
+      - label
+      - input
+      - textarea
+      - copy
+
+The input elements with type="range" have a min, value and max attribute set.
+
+## Responsiveness
+
+### Small screen
+
+For smaller screens e.g smartphone
+
+![](./screenshots/mobile.jpg)
+
+### Big screen
+
+For wide screen sizes like iPad, Laptop and Desktop, the display is flex and flex order is reversed.
+
+On iPads
+
+![](./screenshots/ipad.jpg)
+
+On Laptops
+![](./screenshots/laptop.jpg)
+
+## Event Listeners
+
+All input elements are selected. For each of the input an event listener is added. The event listener works such that on input, the effect is seen on the box shadow or the box itself.
+
+```js
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    renderBox()
+  })
+})
+```
